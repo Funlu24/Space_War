e5fa6f7526b738b0b2ec6e652c8d2364f8b85d14
Funlu24 <funlu8748@gmail.com>
2026-01-04 14:36:01 +0300

    3enimy

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject EnemyPrefab;
+    // [GEREKSİNİM 8] Arrays: Tek bir prefab yerine dizi kullanıyoruz.
+    public GameObject[] EnemyPrefabs; 
 
     public static GameManager instance;
     public float minInstantiateValue;
     public float maxInstantiateValue;
-
     public float enemyDestroyTime = 10f;
 
     [Header("Particle Effects")]
@@ -24,15 +22,10 @@ public class GameManager : MonoBehaviour
 
     private void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); }
     }
+
     void Start()
     {
         StartMenu.SetActive(true);
@@ -40,25 +33,32 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 0f;
         InvokeRepeating("InstantiateEnemy", 1f, 1f);
     }
-private void Update()
+
+    private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             PauseGameButton(true);
         }
     }
-    // Update is called once per frame
+
     void InstantiateEnemy()
     {
         Vector3 Enemypos = new Vector3(Random.Range(minInstantiateValue, maxInstantiateValue), 6f);
-        GameObject enemy = Instantiate(EnemyPrefab, Enemypos, Quaternion.Euler(0, 0, 180f));
+        
+        // [GEREKSİNİM 8] Arrays: Diziden rastgele bir düşman seçimi
+        int randomIndex = Random.Range(0, EnemyPrefabs.Length); 
+        GameObject enemy = Instantiate(EnemyPrefabs[randomIndex], Enemypos, Quaternion.Euler(0, 0, 180f));
+        
         Destroy(enemy, enemyDestroyTime);
     }
+
     public void StartGameButton()
     {
         StartMenu.SetActive(false);
         Time.timeScale = 1f;
     }
+
     public void PauseGameButton(bool isPaused)
     {
         if (isPaused)
--- Assets/Script/PlayerConroller.cs
+++ Assets/Script/PlayerConroller.cs
@@ -4,58 +4,111 @@ using UnityEngine;
 
 public class PlayerConroller : MonoBehaviour
 {
-	public float moveSpeed = 10f;
-[Header("Missile")]
-	public GameObject MissiliePrefab;
-	public Transform MuzzleSpawnPosition;
-	public float DestroyTime = 5f;
+    public float moveSpeed = 10f;
+
+    [Header("Missile")]
+    public GameObject MissiliePrefab;
+    public Transform MuzzleSpawnPosition; // Mermi çıkış noktası ile efekt noktası aynı olabilir
+    public float DestroyTime = 5f;
     public Transform MissileSpawnPoint;
-private void Update()
-{
-		MovePlayer();
-		PlayerShoot();
-	}
 
-	void MovePlayer()
-	{
-		float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-		float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+    [Header("Components")]
+    // [GEREKSİNİM 10] Animator referansı
+    public Animator shipAnimator; 
 
-		transform.Translate(new Vector3(moveX, moveZ, 0));
-	}
-	 void PlayerShoot()
-	{
-		if (Input.GetButtonDown("Jump"))
-		{
-			SpawnMissile();
-			SpawnMuzzleFlash();
-			
-			
-			Debug.Log("Pew Pew!");
-		}
-}
-void SpawnMissile()
-	{
-		GameObject gm = Instantiate(MissiliePrefab, MissileSpawnPoint.position, Quaternion.identity);
-		gm.transform.SetParent(null);
-		Destroy(gm, DestroyTime);
-	}
-	void SpawnMuzzleFlash()
-	{
-		GameObject muzzle = Instantiate(GameManager.instance.MuzzleFlashEffect, MissileSpawnPoint.position, Quaternion.identity);
-		muzzle.transform.SetParent(null);
-		Destroy(muzzle, DestroyTime);
-			
-	}
-		private void OnCollisionEnter2D(Collision2D collision)
-		{
-			if (collision.gameObject.CompareTag("Enemy"))
-			{
-				GameObject gm = Instantiate(GameManager.instance.ParticleEffect, transform.position, Quaternion.identity);
-           Destroy(gm, 2f);
-		   Destroy(this.gameObject);
-				Debug.Log("Player hit by Enemy!");
-			}
-		}
-	
+    private void Update()
+    {
+        MovePlayer();
+        PlayerShoot();
+        CheckEnemyAhead(); // Raycast fonksiyonunu çağırıyoruz
+    }
+
+    // [GEREKSİNİM 6] LateUpdate: Hareket bittikten sonra sınırları kontrol etmek için idealdir.
+    private void LateUpdate()
+    {
+        ClampPosition();
+    }
+
+    void MovePlayer()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // GetAxis raw değeri -1 ile 1 arasındadır.
+        float moveZ = Input.GetAxis("Vertical");
+
+        // [GEREKSİNİM 10] Animator: Hareket varsa animasyonu tetikle (Örn: Motor parlaklığı artar)
+        if(shipAnimator != null)
+        {
+            // "IsMoving" adında bir bool parametresi olduğunu varsayıyoruz.
+            shipAnimator.SetBool("IsMoving", moveX != 0 || moveZ != 0);
+        }
+
+        Vector3 movement = new Vector3(moveX, moveZ, 0) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    // [GEREKSİNİM 6] Ekran sınırlarını belirleme (LateUpdate içinde kullanılır)
+    void ClampPosition()
+    {
+        Vector3 viewPos = transform.position;
+        // X ekseninde ekran dışına çıkmayı engelle (Değerleri sahnene göre ayarla)
+        viewPos.x = Mathf.Clamp(viewPos.x, -2.5f, 2.5f); 
+        viewPos.y = Mathf.Clamp(viewPos.y, -4.5f, 4.5f);
+        transform.position = viewPos;
+    }
+
+    // [GEREKSİNİM 5] RayCast: Öndeki düşmanları algılama sistemi
+    void CheckEnemyAhead()
+    {
+        // Geminin ucundan yukarı doğru bir ışın gönder
+        RaycastHit2D hit = Physics2D.Raycast(MissileSpawnPoint.position, Vector2.up, 10f);
+        
+        // Editörde görmek için kırmızı çizgi çiz
+        Debug.DrawRay(MissileSpawnPoint.position, Vector3.up * 10f, Color.red);
+
+        if (hit.collider != null)
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("Düşman Kilitlendi! Ateş Serbest!");
+                // İleride buraya otomatik ateş etme veya hedef imleci rengi değiştirme eklenebilir.
+            }
+        }
+    }
+
+    void PlayerShoot()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            SpawnMissile();
+            SpawnMuzzleFlash();
+            Debug.Log("Pew Pew!");
+        }
+    }
+
+    void SpawnMissile()
+    {
+        GameObject gm = Instantiate(MissiliePrefab, MissileSpawnPoint.position, Quaternion.identity);
+        gm.transform.SetParent(null);
+        Destroy(gm, DestroyTime);
+    }
+
+    void SpawnMuzzleFlash()
+    {
+        if (GameManager.instance.MuzzleFlashEffect != null)
+        {
+            GameObject muzzle = Instantiate(GameManager.instance.MuzzleFlashEffect, MissileSpawnPoint.position, Quaternion.identity);
+            muzzle.transform.SetParent(null);
+            Destroy(muzzle, DestroyTime);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            GameObject gm = Instantiate(GameManager.instance.ParticleEffect, transform.position, Quaternion.identity);
+            Destroy(gm, 2f);
+            Destroy(this.gameObject);
+            Debug.Log("Player hit by Enemy!");
+        }
+    }
 }
